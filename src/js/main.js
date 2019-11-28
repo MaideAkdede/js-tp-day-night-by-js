9c73a77a86286c7adc89999edaa1712f32ed0add
MaideAkdede <maide.akdede@student.hepl.com>
2019-11-28 11:33:34 +0100

    :sparkles: finishing exercices

--- src/js/main.js
+++ src/js/main.js
@@ -7,26 +7,28 @@
 * 5. Quand on clique une seconde fois il faut retirer les classes que vous venez d'ajouter
 *
 */
-const changeMode = {
-    start(){
-        document.querySelector(".tumbler__wrapper").addEventListener('click', ()=>{
-            this.modeSombre();
+(function (){
+
+    const ChangeMode = {
+        start(){
+            document.documentElement.classList.add('js-enabled');
+            document.querySelector(".tumbler__wrapper").addEventListener('click', ()=>{
+                    this.modeSombre();
+                }
+            );
+        },
+        modeSombre() {
+            //fond du body
+            document.body.classList.toggle('body--night-mode');
+            //bouton image
+            document.querySelector('.tumbler').classList.toggle("tumbler--night-mode");
+            // ligne des articles class .post
+            let articles = document.querySelectorAll('.post');
+            for(let article of articles){
+                article.classList.toggle('post--night-mode');
             }
-        );
-    },
-    modeSombre() {
-        //fond du body
-        document.body.classList.toggle('body--night-mode');
-        //bouton image
-        document.querySelector('.tumbler').classList.toggle("tumbler--night-mode");
-        // ligne des artciles class .post
-        let articles = document.querySelectorAll('.post');
-        for(let article of articles){
-            article.classList.toggle('post--night-mode');
         }
-    }
-};
-
-
-
-changeMode.start();
+    };
+// Appel de la fonction
+    ChangeMode.start();
+})();
